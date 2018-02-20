f67a4bf90180cfc1d84ca77da09e70f5fca1c305
Victor Calderon <victor.calderon90@gmail.com>
2018-02-20 11:03:05 -0600

    Updated 'abundance_matching_f' with new arguments

--- src/data/utilities_python/abundance_matching_vc.py
+++ src/data/utilities_python/abundance_matching_vc.py
@@ -83,37 +83,63 @@ def _monotonic_arrays(x,y):
 		return True
 	else: return False
 
-def abundance_matching_f(dict1, dict2, volume1=1., volume2=1., 
-    reverse=True, dens1_opt=False):
+def abundance_matching_f(dict1, dict2, dict1_names=None, dict2_names=None, 
+    volume1=None, volume2=None, reverse=True, dens1_opt=False, 
+    dens2_opt=False):
     """
-    Does abundance matching based on two quantities (dict1 and dict2).
-    It assigs values from `dict2` to elements in `dict1`.
+    Performs abundance matching based on two quantities (dict1 and dict2).
+    It assigns values from `dict2` to elements in `dict1`.
 
     Parameters
     ----------
     dict1: dictionary_like, or array_like
-        Dictionary of property 1
-        - Keys: `var`: First variable to be analyzed
-                `dens`: Density array corresponding to `var` elements.
-                        Given if dens1==True
+        Dictionary or array-like object of property 1
+        If `dens1_opt == True`:
+            - Object is a dictionary consisting of the following keys:
+                - 'dict1_names': shape (2,)
+                - Order of `dict1_names`: [var1_value, dens1_value]
+        else:
+            - Object is a 1D array or list
+            - Density must be calculated for var2
 
     dict2: dictionary_like, or array_like
-        Dictionary of property 2
-        - Keys: `var`: First variable to be analyzed
-                `dens`: Density array corresponding to `var` elements.
-                        Given if dens1==True
+        Dictionary or array-like object of property 2
+        If `dens2_opt == True`:
+            - Object is a dictionary consisting of the following keys:
+                - 'dict2_names': shape (2,)
+                - Order of `dict2_names`: [var2_value, dens2_value]
+        else:
+            - Object is a 1D array or list
+            - Density must be calculated for var2
 
-    volume1: float
-        volume corresponding to dict1
+    dict1_names: NoneType, or array_list with shape (2,), optional (def: None)
+        names of the `dict1` keys, in order of [var1_value, dens1_value]
+
+    dict2_names: NoneType, or array_list with shape (2,), optional (def: None)
+        names of the `dict2` keys, in order of [var2_value, dens2_value]
+
+    volume1: NoneType or float, optional (default = None)
+        Volume of the 1st variable `var1`
+        Required if `dens1_opt == False`
+
+    volume2: NoneType or float, optional (default = None)
+        Volume of the 2nd variable `var2`
+        Required if `dens2_opt == False`
 
     reverse: boolean
         Determines the relation between var1 and var2.
         - reverse==True: var1 increases with increasing var2
         - reverse==False: var1 decreases with increasing var2
 
-    dens1_opt: boolean, default=False
+    dens1_opt: boolean, optional (default = False)
         - If 'True': density is already provided as key for `dict1`
         - If 'False': density must me calculated
+        - `dict1_names` must be provided and have length (2,)
+
+    dens2_opt: boolean, optional (default = False)
+        - If 'True': density is already provided as key for `dict2`
+        - If 'False': density must me calculated
+        - `dict2_names` must be provided and have length (2,)
 
     Returns
     -------
@@ -121,27 +147,39 @@ def abundance_matching_f(dict1, dict2, volume1=1., volume2=1.,
         numpy.array of elements matching those of `dict1`, after matching with 
         dict2.
     """
-    # 2nd Property
-    var2  = num.array(dict2['var' ])
-    dens2 = num.array(dict2['dens'])
-    if dens1_opt==True:
-        # 1st Property
-        var1  = num.array(dict1['var' ])
-        dens1_1 = num.array(dict1['dens'])
-    elif dens1_opt==False:
-        # 1st Property
-        var1  = dict1.copy()
-        if reverse:
-        # if _reversed_arrays(var2,dens2):
-            dens1 = num.array([num.where(var1<xx)[0].size for xx in var1])+1
-        else:
-            dens1 = num.array([num.where(var1>xx)[0].size for xx in var1])+1
-        dens1 = dens1.astype(float)/volume1
+    ## Checking input parameters
+    # 1st property
+    if dens1_opt:
+        assert(len(dict1_names) == 2)
+        var1  = num.array(dict1[dict1_names[0]])
+        dens1 = num.array(dict1[dict1_names[1]])
     else:
-        raise AssertionError ('Assertion_Error')
-    # Interpolation
-    interp_var2 = interp1d(dens2,var2,bounds_error=True,assume_sorted=False)
-    # Assignment
+        var1        = num.array(dict1)
+        assert(volume1 != None)
+        ## Calculating Density for `var1`
+        if reverse:
+            ncounts1 = num.array([num.where(var1<xx)[0].size for xx in var1])+1
+        else:
+            ncounts1 = num.array([num.where(var1>xx)[0].size for xx in var1])+1
+        dens1 = ncounts1.astype(float)/volume1
+    # 2nd property
+    if dens2_opt:
+        assert(len(dict2_names) == 2)
+        var2  = num.array(dict2[dict2_names[0]])
+        dens2 = num.array(dict2[dict2_names[1]])
+    else:
+        var2        = num.array(dict2)
+        assert(volume2 != None)
+        ## Calculating Density for `var1`
+        if reverse:
+            ncounts2 = num.array([num.where(var2<xx)[0].size for xx in var2])+1
+        else:
+            ncounts2 = num.array([num.where(var2>xx)[0].size for xx in var2])+1
+        dens2 = ncounts2.astype(float)/volume2
+    ##
+    ## Interpolating densities and values
+    interp_var2 = interp1d(dens2, var2, bounds_error=True,assume_sorted=False)
+    # Value assignment
     var1_ab = num.array([interp_var2(xx) for xx in dens1])
 
     return var1_ab

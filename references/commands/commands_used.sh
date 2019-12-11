10865cf1b005d4943550b57b587087d26f91690e
Victor Calderon <2266703+vcalderon2009@users.noreply.github.com>
2019-12-10 22:55:08 -0600

    Updated commands used

--- references/commands/commands_used.sh
+++ references/commands/commands_used.sh
@@ -24,10 +24,10 @@ make clean
 make delete_mock_catls
 
 # Creating new mock catalogues
-make CPU_FRAC=0.75 COSMO_CHOICE="Planck" HALOTYPE="m200b" REMOVE_FILES="True" SURVEY="ECO" catl_mr_make
-make CPU_FRAC=0.75 COSMO_CHOICE="Planck" HALOTYPE="m200b" REMOVE_FILES="True" SURVEY="A" catl_mr_make
-make CPU_FRAC=0.75 COSMO_CHOICE="Planck" HALOTYPE="m200b" REMOVE_FILES="True" SURVEY="B" catl_mr_make
+make CPU_FRAC=0.30 COSMO_CHOICE="Planck" HALOTYPE="m200b" REMOVE_FILES="True" SURVEY="ECO" catl_mr_make
+make CPU_FRAC=0.30 COSMO_CHOICE="Planck" HALOTYPE="m200b" REMOVE_FILES="True" SURVEY="A" catl_mr_make
+make CPU_FRAC=0.30 COSMO_CHOICE="Planck" HALOTYPE="m200b" REMOVE_FILES="True" SURVEY="B" catl_mr_make
 
-make CPU_FRAC=0.75 COSMO_CHOICE="Planck" HALOTYPE="mvir" REMOVE_FILES="True" SURVEY="ECO" catl_mr_make
-make CPU_FRAC=0.75 COSMO_CHOICE="Planck" HALOTYPE="mvir" REMOVE_FILES="True" SURVEY="A" catl_mr_make
-make CPU_FRAC=0.75 COSMO_CHOICE="Planck" HALOTYPE="mvir" REMOVE_FILES="True" SURVEY="B" catl_mr_make
+make CPU_FRAC=0.30 COSMO_CHOICE="Planck" HALOTYPE="mvir" REMOVE_FILES="True" SURVEY="ECO" catl_mr_make
+make CPU_FRAC=0.30 COSMO_CHOICE="Planck" HALOTYPE="mvir" REMOVE_FILES="True" SURVEY="A" catl_mr_make
+make CPU_FRAC=0.30 COSMO_CHOICE="Planck" HALOTYPE="mvir" REMOVE_FILES="True" SURVEY="B" catl_mr_make
